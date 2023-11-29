3e2e81f31468fe9e04e567d011c860c7ccb32747
deny0ng <roden.primero2298@gmail.com>
2023-11-29 21:15:13 +0800

    JONAS BAKLA

--- src/LandingPage/ArticleDetail.jsx
+++ src/LandingPage/ArticleDetail.jsx
@@ -1,42 +1,53 @@
-import { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { NavigationBar } from "../Components/NavigationBar";
 import { Typography } from "@material-tailwind/react";
 import { Footer } from "../Components/Footer";
+import axios from "axios";
 
-export const ArticleDetail = ({ articles }) => {
-    const { articleId } = useParams();
-  
-    // Debugging: Log articleId and articles data
-    console.log('Article ID:', articleId);
-    console.log('Articles Data:', articles);
-  
-    const article = articles.find((article) => article.id === Number(articleId));
-  
-    if (!article) {
-      return <div>Article not found.</div>;
-    }
-  
-    // Render article details here
-    return (
-      <Fragment>
-        <NavigationBar/>
-        <div className="px-52 py-24 flex flex-col gap-5">
-        <img src={article.imagesrc} alt="Article" className="w-full h-[500px] object-cover" />
+export const ArticleDetail = () => {
+  const { intArticleId } = useParams();
+  const [article, setArticle] = useState(null);
+
+  useEffect(() => {
+    const fetchArticle = async () => {
+      try {
+        const response = await axios.get(`https://localhost:44392/api/Article/GetArticleData/${intArticleId}`);
+        setArticle(response.data);
+      } catch (error) {
+        console.error("Error fetching article:", error);
+      }
+    };
+
+    fetchArticle();
+  }, [intArticleId]);
+
+  if (!article) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Fragment>
+      <NavigationBar />
+      <div className="px-52 py-24 flex flex-col gap-5">
+        <img
+          src={article.listArticle[0].photos[0]?.strPhotoUrl || "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"}
+          alt="Article"
+          className="w-full h-[500px] object-cover"
+        />
         <Typography className="text-green font-bold uppercase">
-          {article.category}
+          {article.listArticle[0].strCategory}
         </Typography>
         <Typography variant="h4" color="black">
-          {article.title}
+          {article.listArticle[0].strTitle}
         </Typography>
-        <Typography>{article.description}</Typography>
-        <Typography>Published on: {article.publicationDate}</Typography>
-        <Typography className="font-medium text-light-gray">
-           Writer: {article.writer}
+        <Typography>{article.listArticle[0].strDescription}</Typography>
+        <Typography>Published on: {article.listArticle[0].publicationDate}</Typography>
+        <Typography className="font-medium text-black">
+          Writer: {article.listArticle[0].strWriter}
         </Typography>
-        </div>
-        <Footer/>
-      </Fragment>
-    );
-  };
-  
+      </div>
+      <Footer />
+    </Fragment>
+  );
+};
--- src/LandingPage/Opinion.jsx
+++ src/LandingPage/Opinion.jsx
@@ -1,57 +1,81 @@
-import React, { Fragment } from 'react';
-import { articlesData } from '../Utils/Data';
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
 import { NavigationBar } from '../Components/NavigationBar';
 import { Link } from 'react-router-dom';
 import { Card, CardBody, Typography } from '@material-tailwind/react';
 import { Footer } from '../Components/Footer';
 
-export const Opinion = ({ articles }) => {
-  // Filter the articles array to get only articles with the "Sports" category
-  const opinionArticles = articlesData.filter((article) => article.category === 'Opinion');
+export const Opinion = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get('https://localhost:44392/api/Article/GetArticles');
+        const allArticles = response.data.listArticle;
+        const developmentalCommunicationArticles = allArticles.filter(
+          (article) => article.strCategory === 'Opinion'
+        );
+        setArticles(developmentalCommunicationArticles);
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  if (articles.length === 0) {
+    return (
+      <Fragment>
+        <NavigationBar />
+        <div className="text-center py-20">
+          <Typography>No articles available.</Typography>
+        </div>
+        <Footer />
+      </Fragment>
+    );
+  }
 
   return (
     <Fragment>
-      <NavigationBar/>
+      <NavigationBar />
       <div className="bg-green text-white pt-40 pb-20 text-center">
-      <Typography className="text-6xl font-medium">Opinion</Typography>
-    </div>
+        <Typography className="text-6xl font-medium">OPINION</Typography>
+      </div>
       <div className="px-52 py-24">
-      <h2 className="text-2xl font-bold mb-4">Recent Articles</h2>
-      <ul>
-        {opinionArticles.map((article) => (
-          <li key={article.id}>
-            <Link to={`/article/${article.id}`} className="">
-            <Card className="mt-5  shadow-none hover:bg-light-green-100">
-      <CardBody className='flex flex-row  h-72 gap-10 p-0'>
-      <img
-          src={article.imagesrc}
-          alt="card-image"
-       className='rounded-xl'
-        />
-        <div className='flex flex-col gap-3 justify-center '>
-       <Typography className="text-green font-bold uppercase">
-          {article.category}
-        </Typography>
-        <Typography variant="h4" color="black">
-          {article.title}
-        </Typography>
-        <Typography color="black">
-  {article.description.length > 50 ? `${article.description.substring(0, 200)}...` : article.description}
-</Typography>
-        <Typography className="font-medium text-light-gray">
-           {article.writer}
-        </Typography>
-        </div>
-      
-      </CardBody>
-  
-    </Card>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-    <Footer/>
+        <h2 className="text-2xl font-bold mb-4">Recent Articles</h2>
+        <ul>
+          {articles
+            .filter((article) => article.strCategory === 'Opinion')
+            .map((article) => (
+              <li key={article.intArticleId}>
+                <Link to={`/article/${article.intArticleId}`} className="">
+                  <Card className="mt-5 shadow-none hover:bg-light-green-100">
+                    <CardBody className="flex flex-row h-72 gap-10 p-0">
+                      <img src={article.photos} alt="card-image" className="rounded-xl" />
+                      <div className="flex flex-col gap-3 justify-center">
+                        <Typography className="text-green font-bold uppercase">
+                          {article.strCategory}
+                        </Typography>
+                        <Typography variant="h4" color="black">
+                          {article.strTitle}
+                        </Typography>
+                        <Typography color="black">
+                          {article.strDescription.length > 50
+                            ? `${article.strDescription.substring(0, 200)}...`
+                            : article.strDescription}
+                        </Typography>
+                        <Typography className="font-medium text-light-gray">{article.strWriter}</Typography>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <Footer />
     </Fragment>
   );
 };
